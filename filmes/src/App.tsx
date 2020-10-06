97ddfda06efd04bb0a98546e2d6a66d19dc137bc
Flavio Do Nascimento Pires <39221000826@info.local>
2020-10-06 11:44:56 -0300

    aula1

---
+++ filmes/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/Home/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
